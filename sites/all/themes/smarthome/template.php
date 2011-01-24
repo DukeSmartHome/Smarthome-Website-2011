9b396f763282d0a46627905727094d23601cc437
unknown <Administrator@.(none)>
2011-01-24 15:59:19 -0500

    Configured genesis subtheme.

--- sites/all/themes/smarthome/template.php
+++ sites/all/themes/smarthome/template.php
@@ -6,7 +6,7 @@
  * 1. Rename each function to match your subthemes name,
  *    e.g. if you name your theme "themeName" then the function
  *    name will be "themeName_preprocess_hook". Tip - you can
- *    search/replace on "genesis_SUBTHEME".
+ *    search/replace on "genesis_smarthome".
  * 2. Uncomment the required function to use.
  */
 
@@ -14,9 +14,9 @@
  * Override or insert variables into all templates.
  */
 /* -- Delete this line if you want to use these functions
-function genesis_SUBTHEME_preprocess(&$vars, $hook) {
+function genesis_smarthome_preprocess(&$vars, $hook) {
 }
-function genesis_SUBTHEME_process(&$vars, $hook) {
+function genesis_smarthome_process(&$vars, $hook) {
 }
 // */
 
@@ -24,11 +24,11 @@ function genesis_SUBTHEME_process(&$vars, $hook) {
  * Override or insert variables into the html templates.
  */
 /* -- Delete this line if you want to use these functions
-function genesis_SUBTHEME_preprocess_html(&$vars) {
+function genesis_smarthome_preprocess_html(&$vars) {
   // Uncomment the folowing line to add a conditional stylesheet for IE 7 or less.
   // drupal_add_css(path_to_theme() . '/css/ie/ie-lte-7.css', array('weight' => CSS_THEME, 'browsers' => array('IE' => 'lte IE 7', '!IE' => FALSE), 'preprocess' => FALSE));
 }
-function genesis_SUBTHEME_process_html(&$vars) {
+function genesis_smarthome_process_html(&$vars) {
 }
 // */
 
@@ -36,9 +36,9 @@ function genesis_SUBTHEME_process_html(&$vars) {
  * Override or insert variables into the page templates.
  */
 /* -- Delete this line if you want to use these functions
-function genesis_SUBTHEME_preprocess_page(&$vars) {
+function genesis_smarthome_preprocess_page(&$vars) {
 }
-function genesis_SUBTHEME_process_page(&$vars) {
+function genesis_smarthome_process_page(&$vars) {
 }
 // */
 
@@ -46,9 +46,9 @@ function genesis_SUBTHEME_process_page(&$vars) {
  * Override or insert variables into the node templates.
  */
 /* -- Delete this line if you want to use these functions
-function genesis_SUBTHEME_preprocess_node(&$vars) {
+function genesis_smarthome_preprocess_node(&$vars) {
 }
-function genesis_SUBTHEME_process_node(&$vars) {
+function genesis_smarthome_process_node(&$vars) {
 }
 // */
 
@@ -56,9 +56,9 @@ function genesis_SUBTHEME_process_node(&$vars) {
  * Override or insert variables into the comment templates.
  */
 /* -- Delete this line if you want to use these functions
-function genesis_SUBTHEME_preprocess_comment(&$vars) {
+function genesis_smarthome_preprocess_comment(&$vars) {
 }
-function genesis_SUBTHEME_process_comment(&$vars) {
+function genesis_smarthome_process_comment(&$vars) {
 }
 // */
 
@@ -66,8 +66,8 @@ function genesis_SUBTHEME_process_comment(&$vars) {
  * Override or insert variables into the block templates.
  */
 /* -- Delete this line if you want to use these functions
-function genesis_SUBTHEME_preprocess_block(&$vars) {
+function genesis_smarthome_preprocess_block(&$vars) {
 }
-function genesis_SUBTHEME_process_block(&$vars) {
+function genesis_smarthome_process_block(&$vars) {
 }
 // */
