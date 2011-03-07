9b16a525ecc77f4bf869ce83cba62355b57b8b34
Dean Chen <dean.chen@duke.edu>
2011-03-07 12:33:53 -0500

    Clean up commit.

--- sites/all/themes/smarthome/layouts/home/home.tpl.php
+++ sites/all/themes/smarthome/layouts/home/home.tpl.php
@@ -23,32 +23,36 @@
  *   - $content['bottom']: Content in the bottom row.
  */
 ?>
-<div class="panel-display panel-2col-bricks clearfix" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
+<div class="panel-display panel-homepage clearfix" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
   <div class="panel-panel panel-col-top">
     <div class="inside"><?php print $content['top']; ?></div>
   </div>
-  <div class="center-wrapper">
+  
+  <div class="panel-separator"></div>
+  
+  <div class="middle-wrapper">
     <div class="panel-panel panel-col-first">
-      <div class="inside"><?php print $content['left_above']; ?></div>
+      <div class="inside"><?php print $content['left_middle']; ?></div>
     </div>
 
     <div class="panel-panel panel-col-last">
-      <div class="inside"><?php print $content['right_above']; ?></div>
+      <div class="inside"><?php print $content['right_middle']; ?></div>
     </div>
   </div>
-  <div class="panel-panel panel-col-middle">
-    <div class="inside"><?php print $content['middle']; ?></div>
-  </div>
-  <div class="center-wrapper">
+  
+  <div class="panel-separator"></div>
+  
+  <div class="bottom-wrapper">
     <div class="panel-panel panel-col-first">
-      <div class="inside"><?php print $content['left_below']; ?></div>
+      <div class="inside"><?php print $content['left_bottom']; ?></div>
     </div>
-
+    
+    <div class="panel-panel panel-col">
+      <div class="inside"><?php print $content['center_bottom']; ?></div>
+    </div>
+    
     <div class="panel-panel panel-col-last">
-      <div class="inside"><?php print $content['right_below']; ?></div>
+      <div class="inside"><?php print $content['right_bottom']; ?></div>
     </div>
-  </div>
-  <div class="panel-panel panel-col-bottom">
-    <div class="inside"><?php print $content['bottom']; ?></div>
   </div>
 </div>
